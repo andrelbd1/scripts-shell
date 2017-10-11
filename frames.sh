b7d4b1832785e5d557a3c7ec114545691c97932e
André Brandão <andrelbd1@gmail.com>
2017-10-11 18:45:50 -0300

    first

---
+++ frames.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+location=$(pwd)
+filecount=0
+
+echo "Directory: "$location
+
+for item in $location/*
+do
+  if [ -f "$item" ]
+  then
+    if [ ${item##*.} = "mp4" -o ${item##*.} = "mkv" -o ${item##*.} = "webm" ]; #extension
+    then
+      filecount=$[$filecount+1]
+      echo $item
+      if [ ! -d ${item%.*} ]; #cria diretorio caso nao exista
+      then
+        mkdir ${item%.*}
+      fi
+      cd ${item%.*}
+      duration=$(ffprobe -i $item -show_entries format=duration -v quiet -of csv="p=0")
+      rate=0`echo "scale=2;$duration/100" | bc`;
+      echo $rate
+      ffmpeg -i $item -r 1/$rate -vf scale=255:255 "frame_"%03d.jpg -hide_banner      
+      cd ..
+      #exit 0
+    fi
+  fi
+done
+
+echo $filecount " files"
+
